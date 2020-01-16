0078ab3053ff341b3d52067e5927d0f8fff10ae5
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-16 16:38:58 +0100

    cambio selettore

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-$(".fas.fa-bars").click(function(){
+$(".header-right a").click(function(){
   $(".hamburger-menu").fadeIn(200);
 });
 
 
-$(".fas.fa-times").click(function(){
+$(".hamburger-menu a").click(function(){
   $(".hamburger-menu").fadeOut(200);
 });
